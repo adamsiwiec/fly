fd8cce73eb85b83afe87aa608457b00eb304423a
Adam Siwiec <brickmaster01@gmail.com>
2016-10-06 21:24:35 -0500

    Fixed bugs for -v flag, fixed 403 error, changed from request to needle

--- fly.js
+++ fly.js
@@ -1,18 +1,40 @@
 #! /usr/bin/env node
 
-
-var request = require('request');
+var needle = require('needle');
 var opener = require('opener');
-var flags = require('flags');
 var pack = require('./package.json');
 
 
+function fly(type, repo) {
+
+    if (type === "person") {
+        var person = type;
+        var requestUrl = "users";
+    } else if (type === "repo") {
+        var person = type;
+        var requestUrl = "repos";
+    }
+    var url = "https://api.github.com/" + requestUrl + "/" + repo;
 
 
-flags.defineBoolean('version');
-flags.parse();
+    needle.get(url, function(err, res) {
+        if (!err && res.statusCode === 200) {
 
-if (flags.get('version')) {
+            var link = "https://github.com/" + repo;
+            opener(link);
+            process.exit();
+
+        } else {
+            console.log('This ' + person + ' does not exist');
+            process.exit();
+        }
+
+    });
+
+}
+
+
+if (process.argv.indexOf('--version') != -1 || process.argv.indexOf('-v') != -1) {
     console.log(pack.version);
     process.exit();
 }
@@ -24,43 +46,16 @@ console.log('');
 var repoSlice = process.argv.slice(2);
 var repo = repoSlice[0];
 
-
 if (!repo) {
     console.log("You have to give me a repo to look for!");
     console.log("Try 'fly adamsiwiec/fly' ");
     process.exit();
 }
 
+if (repo.indexOf('/') !== -1) {
 
-if (repo.indexOf('/')) {
+    fly("repo", repo);
 
-    request('https://api.github.com/repos/' + repo, function(err, res, body) {
-        if (!err && res.statusCode == 200) {
-            var link = "https://github.com/" + repo;
-            opener(link);
-            process.exit();
-
-        }
-        console.log('This repo does not exist');
-        process.exit();
-
-    });
-
-
-}
-
-else {
-
-    request('https://api.github.com/users/' + repo, function(err, res, body) {
-        if (!err && res.statusCode == 200) {
-            var link = "https://github.com/" + repo;
-            opener(link);
-            process.exit();
-
-        }
-        console.log('This person does not exist');
-        process.exit();
-
-    });
-
+} else {
+    fly("person", repo);
 }
