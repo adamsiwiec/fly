a169f4476acb2e867836777339a9d0301b9431d9
Adam Siwiec <brickmaster01@gmail.com>
2016-11-23 18:19:12 -0600

    bumped dependencies

--- fly-cli.js
+++ fly-cli.js
@@ -1,30 +1,26 @@
 #!/usr/bin/env node
 
-var fly = require("./fly.js");
-var pack = require("./package.json")
+var fly = require('./fly.js');
+var pack = require('./package.json');
 
 if (process.argv.indexOf('--version') != -1 || process.argv.indexOf('-v') != -1) {
-    console.log(pack.version);
-    process.exit();
+	console.log(pack.version);
+	process.exit();
 }
 
-
 console.log('');
-
 
 var repoSlice = process.argv.slice(2);
 var repo = repoSlice[0];
 
 if (!repo) {
-    console.log("You have to give me a repo to look for!");
-    console.log("Try 'fly adamsiwiec/fly' ");
-    process.exit();
+	console.log('You have to give me a repo to look for!');
+	console.log('Try \'fly adamsiwiec/fly\' ');
+	process.exit();
 }
 
 if (repo.indexOf('/') !== -1) {
-
-    fly.fly("repo", repo);
-
+	fly.fly('repo', repo);
 } else {
-    fly.fly("person", repo);
+	fly.fly('person', repo);
 }
--- fly.js
+++ fly.js
@@ -1,11 +1,34 @@
 var needle = require('needle');
 var opener = require('opener');
+var GitHubApi = require('github');
 
 var exports = module.exports = {};
 
 exports.fly = function (type, repo) {
+	var github = new GitHubApi({
+	    // optional args
+
+	    protocol: 'https',
+	    host: 'api.github.com', // should be api.github.com for GitHub
+	    headers: {
+	        'user-agent': 'adamsiwiec' // GitHub is happy with a unique user agent
+	    },
+	    followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+	    timeout: 5000
+	});
+
 	if (type === 'person') {
-			var person = type;
+
+		github.users.getForUser({
+    user: repo
+}, function(err, res) {
+	if (err) {
+		console.log(err);
+	}
+	var user = JSON.stringify(res);
+    console.log(user);
+});
+		var person = type;
 		var requestUrl = 'users';
 	} else if (type === 'repo') {
 		var person = type;
@@ -14,7 +37,7 @@ exports.fly = function (type, repo) {
 	var url = 'https://api.github.com/' + requestUrl + '/' + repo;
 
 	needle.get(url, function (err, res) {
-			if (!err && res.statusCode === 200) {
+		if (!err && res.statusCode === 200) {
 			var link = 'https://github.com/' + repo;
 			opener(link);
 
