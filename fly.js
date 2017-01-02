e6edea09c4973aff3e331731328acbd4815a0b6b
Adam Siwiec <brickmaster01@gmail.com>
2017-01-02 14:49:31 -0600

    Minor text fixes

--- fly.js
+++ fly.js
@@ -1,53 +1,51 @@
-var needle = require('needle');
-var opener = require('opener');
-var GitHubApi = require('github');
+	var needle = require('needle');
+	var opener = require('opener');
+	var GitHubApi = require('github');
 
-var exports = module.exports = {};
+	var exports = module.exports = {};
 
-exports.fly = function (type, repo) {
-	var github = new GitHubApi({
-	    // optional args
+	exports.fly = function(type, repo) {
+	    var github = new GitHubApi({
+	        // optional args
 
-	    protocol: 'https',
-	    host: 'api.github.com', // should be api.github.com for GitHub
-	    headers: {
-	        'user-agent': 'adamsiwiec' // GitHub is happy with a unique user agent
-	    },
-	    followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
-	    timeout: 5000
-	});
+	        protocol: 'https',
+	        host: 'api.github.com', // should be api.github.com for GitHub
+	        headers: {
+	            'user-agent': 'adamsiwiec' // GitHub is happy with a unique user agent
+	        },
+	        followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+	        timeout: 5000
+	    });
 
-	if (type === 'person') {
+	    if (type === 'person') {
+	        github.users.getForUser({
+	            user: repo
+	        }, function(err, res) {
+	            if (err) {
+	                console.log(err);
+	            }
+	            var user = JSON.stringify(res);
+	        });
+	        var person = type;
+	        var requestUrl = 'users';
+	    } else if (type === 'repo') {
+	        var person = type;
+	        var requestUrl = 'repos';
+	    }
+	    var url = 'https://api.github.com/' + requestUrl + '/' + repo;
 
-		github.users.getForUser({
-    user: repo
-}, function(err, res) {
-	if (err) {
-		console.log(err);
-	}
-	var user = JSON.stringify(res);
-    console.log(user);
-});
-		var person = type;
-		var requestUrl = 'users';
-	} else if (type === 'repo') {
-		var person = type;
-		var requestUrl = 'repos';
-	}
-	var url = 'https://api.github.com/' + requestUrl + '/' + repo;
+	    needle.get(url, function(err, res) {
+	        if (!err && res.statusCode === 200) {
+	            var link = 'https://github.com/' + repo;
+	            opener(link);
 
-	needle.get(url, function (err, res) {
-		if (!err && res.statusCode === 200) {
-			var link = 'https://github.com/' + repo;
-			opener(link);
+	            process.exit();
+	        } else {
+	            console.log('This ' + person + ' does not exist');
 
-			process.exit();
-		} else {
-			console.log('This ' + person + ' does not exist');
+	            process.exit();
+	        }
+	    });
 
-			process.exit();
-		}
-	});
-
-	return true;
-};
+	    return true;
+	};
